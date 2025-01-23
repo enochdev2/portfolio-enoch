6bf02604b37cb255008a36be7f9e78bdc0eb8ee7
enochdev2 <enochthechristepistle@gmail.com>
2025-01-23 15:16:20 +0100

    first commit

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -9,7 +9,7 @@ const Tech = () => {
 
   useEffect(() => {
     // Add a listener for changes to the screen size
-    const mediaQuery = window.matchMedia("(max-width: 600px)");
+    const mediaQuery = window.matchMedia("(max-width: 700px)");
 
     // Set the initial value of the `isMobile` state variable
     setIsMobile(mediaQuery.matches);
